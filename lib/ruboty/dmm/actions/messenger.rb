e0fc1848e9705a771af3276373b5e81268d0f5df
Satoshi Ohmori <sachin21dev@gmail.com>
2017-02-25 03:52:24 +0900

    Support all rankings

--- lib/ruboty/dmm/actions/messenger.rb
+++ lib/ruboty/dmm/actions/messenger.rb
@@ -3,7 +3,7 @@ module Ruboty
     module Actions
       class Messenger < Ruboty::Actions::Base
         def call
-          ranking = Ruboty::DMM::Ranking.new(message.body).call
+          ranking = Ruboty::DMM::Ranking.new(submedia: message.match_data[:submedia], term: message.match_data[:term]).call
           message.reply(ranking)
         rescue => exception
           message.reply("Failed by #{exception.class}")
