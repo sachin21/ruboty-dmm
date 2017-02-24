db7050258cf3dc6dc1c3666601d89f6d09a17ccc
Satoshi Ohmori <sachin21dev@gmail.com>
2017-02-24 21:53:46 +0900

    Change fetcher klass to testable

--- lib/ruboty/dmm.rb
+++ lib/ruboty/dmm.rb
@@ -1,5 +1,7 @@
 require 'mechanize'
 
-require 'ruboty/dmm/actions/fetcher'
+require 'ruboty/dmm/agent'
+require 'ruboty/dmm/ranking'
+require 'ruboty/dmm/actions/messenger'
 require 'ruboty/dmm/version'
 require 'ruboty/handlers/dmm'
